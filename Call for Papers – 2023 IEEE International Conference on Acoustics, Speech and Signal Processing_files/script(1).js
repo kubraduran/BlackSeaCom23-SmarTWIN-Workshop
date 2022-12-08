a480a81171ed3b316d50fd35697436c4feda227b
Elif Ak <akeli@itu.edu.tr>
2022-12-08 12:57:41 +0300

    Add files via upload

---
+++ Call for Papers – 2023 IEEE International Conference on Acoustics, Speech and Signal Processing_files/script(1).js
@@ -0,0 +1,14 @@
+(function (w) {
+    
+        
+    if (typeof w.feathr === 'function') {
+        w.feathr('integrate', 'ttd', '629d0135dbe7a10009ca3b50');
+    }
+        
+        
+    if (typeof w.feathr === 'function') {
+        w.feathr('match', '629d0135dbe7a10009ca3b50');
+    }
+        
+    
+}(window));
